fea161c3fd29e6c44d9bb4f1f23ccaf98c0efddb
NestorJMartinez <nmarti62@calpoly.edu>
2021-12-28 22:19:28 -0800

    second prettier test

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const test_prettier = "writing a test statement that will be very long to see if prettier will automatically wrap it for me"
+const test_prettier = "writing a test statement that will be very long to see if prettier will automatically wrap it for me" + "second string"
 /*
  * code goes here :)
  */
