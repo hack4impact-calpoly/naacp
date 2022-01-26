9b4f9dc4dc3618867bcba461f48f69b32bdac8eb
Storm Randolph <storm.randolph@gmail.com>
2022-01-26 13:28:57 -0800

    Updated Garden Schema to make desired changes

--- backend/models/garden.ts
+++ backend/models/garden.ts
@@ -1,24 +1,29 @@
 import {Schema, Model, Types} from 'mongoose';
+import {gardenConnection} from '../connection'
 
-'creating document interface for typescri'
-interface User{
+'creating document interface for typescript'
+interface IGarden{
     name: string;
     description: string;
     location: string;
-    pictures: Types.array<string>;
-    gardeners: Types.array<string>;
+    pictures: Types.Array<string>;
+    gardeners: Types.Array<string>;
     community: boolean;
-    plants: Types.array<string>;
+    plants: Types.Array<string>;
     date: Date;
 }
 
 'creating schema for gardens'
-const schema = new Schema<User, Model<User>>({
+const schema = new Schema<IGarden, Model<IGarden>>({
     name: { type: String, required: true },
     description: { type: String, required: true },
+    location: { type: String, required: true },
     pictures:[String],
     gardeners: [String],
     community: { type: Boolean, required: true },
     plants: [String],
     date: { type: Date, required: true }
 });
+
+const Garden = gardenConnection.model('gardenDB', schema)
+export default Garden;
