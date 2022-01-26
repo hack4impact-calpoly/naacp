f2ed9c6e55398e970b88e4b80ef2e97c832c3693
Shad <shad12@outlook.com>
2022-01-25 22:24:13 -0800

    added basis of username + login fields

--- frontend/src/components/Login/login.js
+++ frontend/src/components/Login/login.js
@@ -4,6 +4,24 @@ function Login() {
   return (
     <div className="Login">
       <p>Login</p>
+      <div>
+        <label htmlFor="" className="text-sm font-bold text-gray-600 block">
+          Username
+        </label>
+        <input
+          type="text"
+          className="w-full p=2 border border-gray-300 rounded mt-1"
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="" className="text-sm font-bold text-gray-600 block">
+          Password
+        </label>
+        <input
+          type="password"
+          className="w-full p=2 border border-gray-300 rounded mt-1"
+        ></input>
+      </div>
     </div>
   );
 }
