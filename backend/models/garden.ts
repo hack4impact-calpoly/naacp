ccf619ac172b49b55d624905cfee991cd4a317d4
NestorJMartinez <43479997+NestorJMartinez@users.noreply.github.com>
2022-01-31 00:34:19 -0800

    fixed env file, and mongodb connections

--- backend/models/garden.ts
+++ backend/models/garden.ts
@@ -1,29 +1,32 @@
-import {Schema, Model, Types} from 'mongoose';
-import {gardenConnection} from '../connection'
+import { Schema, Model, Types } from "mongoose";
+import { gardenConnection } from "../connection";
 
-'creating document interface for typescript'
-interface IGarden{
-    name: string;
-    description: string;
-    location: string;
-    pictures: Types.Array<string>;
-    gardeners: Types.Array<string>;
-    community: boolean;
-    plants: Types.Array<string>;
-    date: Date;
+("creating document interface for typescript");
+interface IGarden {
+  name: string;
+  description: string;
+  location: string;
+  pictures: Types.Array<string>;
+  gardeners: Types.Array<string>;
+  community: boolean;
+  plants: Types.Array<string>;
+  date: Date;
 }
 
-'creating schema for gardens'
-const schema = new Schema<IGarden, Model<IGarden>>({
+("creating schema for gardens");
+const schema = new Schema<IGarden, Model<IGarden>>(
+  {
     name: { type: String, required: true },
     description: { type: String, required: true },
     location: { type: String, required: true },
-    pictures:[String],
+    pictures: [String],
     gardeners: [String],
     community: { type: Boolean, required: true },
     plants: [String],
-    date: { type: Date, required: true }
-});
+    date: { type: Date, required: true },
+  },
+  { collection: "gardenDB" }
+);
 
-const Garden = gardenConnection.model('gardenDB', schema)
+const Garden = gardenConnection.model("gardenDB", schema);
 export default Garden;
