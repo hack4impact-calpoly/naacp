766e3c7f149538209363a021c5aecce67ae0ecfb
Shad <shad12@outlook.com>
2022-02-07 18:24:42 -0800

    fixed login code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home/home";
-import Login from "./components/Login/login";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 import Navigation from "./navigation";
 
 export default function App() {
