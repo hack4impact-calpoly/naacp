c9c21a54c7baac55ba1f889a9ba54f7845aa762e
esauerbo <emma.m.sauerborn@gmail.com>
2022-02-09 19:53:15 -0800

    added bare bones navbar with tailwind.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Navigation from "./navigation";
+import NavBar from "./components/NavBar/NavBar";
+import "./App.css";
 
 export default function App() {
   return (
@@ -14,6 +16,7 @@ export default function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/nav" element={<NavBar />} />
             </Routes>
           </ul>
         </nav>
