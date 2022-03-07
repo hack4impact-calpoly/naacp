bf6c5c144fb53854a8a4de1ac0b489c3c6744981
Shad <shad12@outlook.com>
2022-03-07 15:26:41 -0800

    looks goodish

--- frontend/src/components/GardenCard/GardenCard.tsx
+++ frontend/src/components/GardenCard/GardenCard.tsx
@@ -1,19 +1,15 @@
 import React from "react";
 //import { Row, Col, Card } from "react-bootstrap";
+import "./GardenCard.css";
 
 export default function GardenCard() {
   return (
     <body>
       <div className="md:container md:mx-auto py-20">
-        <div className="card md:mx-auto py-5 px-5" style={{ width: "50%" }}>
+        <div className="card md:mx-auto py-5 px-5" style={{ width: "500px" }}>
           <div className="row">
             <div className="col">
-              <img
-                width={200}
-                height={150}
-                className="cardImage"
-                src={require("./garden.jpg")}
-              />
+              <img width={150} src={require("./garden.jpg")} />
             </div>
             <div className="col">
               <h5 className="card-title">City Garden</h5>
@@ -31,11 +27,7 @@ export default function GardenCard() {
               <div className="card-body">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                enim ad minim veniam, quis nos...
               </div>
             </div>
           </div>
