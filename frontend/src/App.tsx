9e816fa9f2611965c216b381ed7b26c3ab1b0323
NestorJMartinez <43479997+NestorJMartinez@users.noreply.github.com>
2022-05-11 14:51:36 -0700

    moved awsconfig info

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,9 +10,32 @@ import Footer from "./components/Footer/Footer";
 import GardenCard from "./components/GardenCard/GardenCard";
 import "./App.css";
 import { Auth } from "aws-amplify";
-import awsconfig from "./aws-exports";
 
-Auth.configure(awsconfig);
+const awsmobile = {
+  aws_project_region: "us-west-2",
+  aws_cognito_identity_pool_id:
+    "us-west-2:7d812fda-4620-431f-bf0f-769f0a2f8272",
+  aws_cognito_region: "us-west-2",
+  aws_user_pools_id: "us-west-2_dj6fZhBti",
+  aws_user_pools_web_client_id: "5aphvj67gi3p9ehiq94lvqcib4",
+  oauth: {},
+  aws_cognito_username_attributes: ["EMAIL"],
+  aws_cognito_social_providers: [],
+  aws_cognito_signup_attributes: ["EMAIL"],
+  aws_cognito_mfa_configuration: "OFF",
+  aws_cognito_mfa_types: ["SMS"],
+  aws_cognito_password_protection_settings: {
+    passwordPolicyMinLength: 6,
+    passwordPolicyCharacters: [
+      "REQUIRES_LOWERCASE",
+      "REQUIRES_NUMBERS",
+      "REQUIRES_UPPERCASE",
+    ],
+  },
+  aws_cognito_verification_mechanisms: ["EMAIL"],
+};
+
+Auth.configure(awsmobile);
 
 export default function App() {
   return (
